04440fbba638976499fea5509a542cce87b80d22
Tomasz Jaskula <thomasv1000@hotmail.fr>
2024-11-22 22:06:47 +0100

    Add application Window tutorial

---
+++ TheApplicationWindow/MainApplicationWindow/Source/Main.cpp
@@ -0,0 +1,77 @@
+/*
+  ==============================================================================
+
+    This file contains the basic startup code for a JUCE application.
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+
+//==============================================================================
+class MainApplicationWindowApplication  : public juce::JUCEApplication
+{
+public:
+    //==============================================================================
+    MainApplicationWindowApplication() {}
+
+    const juce::String getApplicationName() override       { return ProjectInfo::projectName; }
+    const juce::String getApplicationVersion() override    { return ProjectInfo::versionString; }
+    bool moreThanOneInstanceAllowed() override             { return true; }
+
+    //==============================================================================
+    void initialise (const juce::String& commandLine) override
+    {
+        // Add your application's initialisation code here..
+        mainWindow.reset (new MainWindow (getApplicationName()));
+    }
+
+    void shutdown() override
+    {
+        // Add your application's shutdown code here..
+        mainWindow = nullptr;
+    }
+
+    //==============================================================================
+    void systemRequestedQuit() override
+    {
+        // This is called when the app is being asked to quit: you can ignore this
+        // request and let the app carry on running, or call quit() to allow the app to close.
+        quit();
+    }
+
+    void anotherInstanceStarted (const juce::String& commandLine) override
+    {
+        // When another instance of the app is launched while this one is running,
+        // this method is invoked, and the commandLine parameter tells you what
+        // the other instance's command-line arguments were.
+    }
+    //==============================================================================
+    class MainWindow    : public juce::DocumentWindow
+    {
+    public:
+        MainWindow (juce::String name)  : DocumentWindow (name,
+                                                          juce::Colours::lightgrey,
+                                                          DocumentWindow::allButtons)
+        {
+            centreWithSize (300, 200);
+            setVisible (true);
+            setResizable (true, true);
+            setUsingNativeTitleBar (true);
+        }
+
+        void closeButtonPressed() override
+        {
+            juce::JUCEApplication::getInstance()->systemRequestedQuit();
+        }
+
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+    };
+private:
+    std::unique_ptr<MainWindow> mainWindow;
+};
+
+//==============================================================================
+// This macro generates the main() routine that launches the app.
+START_JUCE_APPLICATION (MainApplicationWindowApplication)
