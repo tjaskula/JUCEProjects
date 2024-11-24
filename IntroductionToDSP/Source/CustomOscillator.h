621ea8b672c831f92eadd263b3f4490a1c9cfb78
Tomasz Jaskula <thomasv1000@hotmail.fr>
2024-11-24 23:28:35 +0100

    Sawtooth Tutorial: Introduction to DSP

--- IntroductionToDSP/Source/CustomOscillator.h
+++ IntroductionToDSP/Source/CustomOscillator.h
@@ -10,8 +10,19 @@ public:
     //==============================================================================
     CustomOscillator()
     {
+//        auto& osc = processorChain.template get<oscIndex>();
+//        osc.initialise ([] (Type x) { return std::sin (x); }, 128);
+
+        // sawtooth
         auto& osc = processorChain.template get<oscIndex>();
-        osc.initialise ([] (Type x) { return std::sin (x); }, 128);
+        osc.initialise ([] (Type x)
+                        {
+                            return juce::jmap (x,
+                                               Type (-juce::MathConstants<double>::pi),
+                                               Type (juce::MathConstants<double>::pi),
+                                               Type (-1),
+                                               Type (1));
+                        }, 2);
     }
 
     //==============================================================================
